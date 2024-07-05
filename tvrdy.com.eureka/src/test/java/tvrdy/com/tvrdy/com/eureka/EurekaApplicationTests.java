d189b8b9c8b8d0b87861b163b9b82cfde6207430
tvrdypeter <tvrdy.peter@gmx.de>
2024-07-05 17:31:21 +0200

    First commit - commiting all files to the repository

---
+++ tvrdy.com.eureka/src/test/java/tvrdy/com/tvrdy/com/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package tvrdy.com.tvrdy.com.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
